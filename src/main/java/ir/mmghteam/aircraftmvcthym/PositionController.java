05924069d1c89ff6e4c191d568cc65874998eaba
MMGHTeam <mmgh102@gmail.com>
2023-05-16 16:18:44 +0330

    Update code with WebSocket support not working properly yet

--- src/main/java/ir/mmghteam/aircraftmvcthym/PositionController.java
+++ src/main/java/ir/mmghteam/aircraftmvcthym/PositionController.java
@@ -5,28 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @RequiredArgsConstructor
 @Controller
 public class PositionController {
     @NonNull
     private final AircraftRepository repository;
-    private WebClient  webClient = WebClient.create("http://localhost:7634/aircraft");
 
     @GetMapping("/aircraft")
-    public String getCurrentAircraftPosition(Model model) {
-        repository.deleteAll();
-
-        webClient.get()
-                .retrieve()
-                .bodyToFlux(Aircraft.class)
-                .filter(plane -> !plane.getReg().isEmpty())
-                .toStream()
-                .forEach(repository::save);
-
+    public String getCurrentAircraftPositions(Model model) {
         model.addAttribute("currentPositions", repository.findAll());
         return "positions";
     }
-
 }
---
+++ src/main/java/ir/mmghteam/aircraftmvcthym/WebSocketHandler.java
@@ -0,0 +1,57 @@
+package ir.mmghteam.aircraftmvcthym;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+    private final List<WebSocketSession> sessionList = new ArrayList<>();
+    @NotNull
+    private final AircraftRepository repository;
+
+    public List<WebSocketSession> getSessionList() {
+        return sessionList;
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessionList.add(session);
+        System.out.println("Connection established from " + session.toString() +
+                " @ " + Instant.now().toString());
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message)
+            throws Exception {
+        try {
+            System.out.println("Message received : '" + message + "from " + session.toString());
+            for (WebSocketSession sessionInList : sessionList) {
+                if (sessionInList != session) {
+                    sessionInList.sendMessage(message);
+                    System.out.println("--> Sending Message '" + message +
+                            "' to " + sessionInList.toString());
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Exception Handling message: " + e.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status)
+            throws Exception {
+        sessionList.remove(session);
+        System.out.println("Connection closed by " + session.toString() +
+                " @ " + Instant.now().toString());
+    }
+}
